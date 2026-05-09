df3f4dc326d0b9cbf3c669225970cc9596d01895
hossamhosam820 <hossamhosam820@gmail.com>
2026-05-09 16:32:23 +0300

    I am Hacker

--- src/main/java/com/elhackarz/fehu2026/dto/SignupRequest.java
+++ src/main/java/com/elhackarz/fehu2026/dto/SignupRequest.java
@@ -1,11 +1,22 @@
 package com.elhackarz.fehu2026.dto;
+import java.time.LocalDate;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Data;
-
 @Data
 public class SignupRequest {
     private String name;
     private String username;
     private String email;
     private String password;
+    @DecimalMax(value = "4.0", message = "GPA cannot be more than 4")
+    private Double gpa;
+    private LocalDate birthDate;
+@Min(value = 1, message = "Academic year cannot be less than 1")
+@Max(value = 4, message = "Academic year cannot be more than 4")
+    private Integer acyear; //Acadimic year ya sohaib
+
+
 }
