214e710c932042d7e9414c44f00e6b9c97ea0206
SuchATypicalDev <120500789+SuchATypicalDev@users.noreply.github.com>
2026-05-09 16:31:20 -0300

    Update SignupRequest.java

--- src/main/java/com/elhackarz/fehu2026/dto/SignupRequest.java
+++ src/main/java/com/elhackarz/fehu2026/dto/SignupRequest.java
@@ -14,9 +14,4 @@ public class SignupRequest {
     @DecimalMax(value = "4.0", message = "GPA cannot be more than 4")
     private Double gpa;
     private LocalDate birthDate;
-@Min(value = 1, message = "Academic year cannot be less than 1")
-@Max(value = 4, message = "Academic year cannot be more than 4")
-    private Integer acyear; //Acadimic year ya sohaib
-
-
 }
